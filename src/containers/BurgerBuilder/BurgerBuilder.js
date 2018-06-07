b8677c10d0b2f5680297eea2eac8b47352446259
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2018-06-07 20:08:03 +0300

    Added displaying and updating the burger price

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -65,6 +65,7 @@ class BurgerBuilder extends Component {
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemove={this.removeIngredientHandler}
                     disabled={disableInfo}
+                    price={this.state.totalPrice}
                 />
             </Auxiliary>
         );
