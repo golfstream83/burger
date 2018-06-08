d8f811ce8fe881458375b2ce84db295d06a62106
Тулин Виктор Владимирович <VVTulin.OUT@sberbank.ru>
2018-06-08 11:05:06 +0300

    Added display logic for Backdrop component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: true});
     };
 
+    purchaseCancelHandler = () => {
+        this.setState({purchasing: false})
+    };
+
     render () {
         const disableInfo = {
             ...this.state.ingredients
@@ -81,7 +85,9 @@ class BurgerBuilder extends Component {
         }
         return (
             <Auxiliary>
-                <Modal show={this.state.purchasing}>
+                <Modal
+                    show={this.state.purchasing}
+                    modalClosed={this.purchaseCancelHandler}>
                     <OrderSummury ingredients={this.state.ingredients}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
